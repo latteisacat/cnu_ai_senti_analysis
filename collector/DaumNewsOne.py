faced5f55691cd3dc929367ab48f2385b751a5e2
임도현 <dragonldh0907@gmail.com>
2022-10-04 11:47:25 +0900

    최초등록

---
+++ collector/DaumNewsOne.py
@@ -0,0 +1,9 @@
+# 1.프로젝트(cnu_ai_senti_analysis-main)
+# ┖2.python package(collector)
+#  ┖3.Python file(test.py, DaumNewsOne.py)
+# - python package: python file들을 모아두는 폴더
+#                           폴더아이콘안에 구멍 뚫려있음
+
+
+import requests
+from bs4 import BeautifulSoup
